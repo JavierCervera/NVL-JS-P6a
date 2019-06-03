adc6b30be92e40ee6cecdea8689a64bc1853489a
JavierCervera <43016229+JavierCervera@users.noreply.github.com>
2019-06-03 21:52:02 +0200

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,6 @@
+var miNombre = "Javier Cervera Garcia";
+var miEdad = 47;
+var diasSemana = "Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo";
+var gravedad = 9.8;
+var primos = "1, 2, 3";
+alert(miNombre);
